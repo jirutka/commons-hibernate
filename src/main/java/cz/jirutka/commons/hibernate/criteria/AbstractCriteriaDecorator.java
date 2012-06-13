2cd6d09a354df9d9979166697bf8605ce3934f56
Jakub Jirutka <jakub@jirutka.cz>
2012-06-14 01:43:56 +0200

    Move AbstractCriteriaDecorator to subpackage!

--- src/main/java/cz/jirutka/commons/hibernate/criteria/AbstractCriteriaDecorator.java
+++ src/main/java/cz/jirutka/commons/hibernate/criteria/AbstractCriteriaDecorator.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package cz.jirutka.commons.hibernate;
+package cz.jirutka.commons.hibernate.criteria;
 
 import java.util.List;
 import org.hibernate.CacheMode;
